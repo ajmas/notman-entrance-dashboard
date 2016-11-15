9facf7ed8c68623fa035494bd3d3d34ef19091b4
Andre Mas <andrejohn.mas@gmail.com>
2016-11-14 22:38:02 -0500

    Chnages

--- bin/deploy-netlify.sh
+++ bin/deploy-netlify.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-echo "Before set"
-
 set -e # halt script on error
 
-echo "Creating website.zip from build directory"
+echo "info: Creating website.zip from build directory"
 zip -r website.zip build 
 
-echo "Sending website.zip to Netlify site ${NETLIFY_SITE}"
+echo "info: Sending website.zip to Netlify site ${NETLIFY_SITE}"
 
 curl -H "Content-Type: application/zip" \
     -H "Authorization: Bearer $NETLIFY_KEY" \
     --data-binary "@website.zip" \
     "https://api.netlify.com/api/v1/sites/${NETLIFY_SITE}/deploys"
 
-echo "Finished deploying to Netlify"
+echo "info: Finished deploying to Netlify"
